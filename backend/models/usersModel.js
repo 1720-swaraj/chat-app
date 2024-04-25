225d45cd9a44755fc828b69ebbebd6c0940dd773
swaraj kadam <swarajkadam45@gmail.com>
2024-04-25 13:50:38 +0530

    user schena added

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -1,5 +1,28 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 export const userSchema = mongoose.Schema({
-        
-})
+  fullName: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePhoto: {
+    type: String,
+    default: "",
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true,
+  },
+},{
+    timestamps:true
+});
