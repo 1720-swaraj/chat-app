7eedac56c89046f3c9a2d347c661e0a4ccc1b60d
swaraj kadam <swarajkadam45@gmail.com>
2024-04-30 14:20:25 +0530

    messege pass testing

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies?.token;
     if (!token) {
       return res.status(401).json({ message: "user not authenticated" });
     }
